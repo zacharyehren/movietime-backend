96ef68cc01a1be1753ae8cdc4516d172c65770b2
Zach Ehren <zehren@cisco.com>
2019-11-08 15:11:16 -0800

    add axios for HTTP requests

--- api/services/omdb.service.js
+++ api/services/omdb.service.js
@@ -1,7 +1,13 @@
+const axios = require('axios');
 const ApiDetails = require('../apidetails')
 const uri = "http://www.omdbapi.com/?"
 const key = ApiDetails.apiKey;
 
-const queryApi = (search) => {
-
-}
+const query = axios({
+    method: 'get',
+    url: '/user/12345',
+    data: {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    }
+  });
