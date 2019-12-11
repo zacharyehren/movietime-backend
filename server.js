fa30d713b5689d1d1e22cdc2fdbde0517119da8c
Zach Ehren <zehren@cisco.com>
2019-12-11 09:09:58 -0800

    wire up routes to server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+MovieList = require('./api/models/movieListModel');
+bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+const routes = require('./api/routes/movieList.endpoint');
+routes(app);
 
 app.listen(port);
 
