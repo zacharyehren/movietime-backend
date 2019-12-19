d1ff6e50598d801030f8555a4981625f3b4d92ed
Zach Ehren <zehren@cisco.com>
2019-12-19 09:07:37 -0800

    return repsonse in handler

--- api/routes/movieList.handler.js
+++ api/routes/movieList.handler.js
@@ -1,7 +1,8 @@
 const omdbService = require('../services/omdb.service.js');
 
-const searchForMovie = (request) => {
-    omdbService.getMovie(request.params.searchQuery);
+const searchForMovie = async (request, response) => {
+    const movies = await omdbService.getMovie(request.params.searchQuery);
+    return response.json(movies.data);
 }
 
 module.exports = {
